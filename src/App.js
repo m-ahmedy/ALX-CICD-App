2bea644b71e3f15c0c3030b3332468819579fe16
Mahmoud Ahmedy <m7m9d.a7medy@gmail.com>
2023-01-14 16:10:40 +0200

    Source code change

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Welcome to the CI/CD App</p>
+        <p>We are ALX Prime</p>
       </header>
     </div>
   );
